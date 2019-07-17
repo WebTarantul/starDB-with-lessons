87e7fd9f1c229e23bfb9354fb9f8efda1581cd21
WebTarantul <tarstar1990@gmail.com>
2019-07-17 17:27:18 +0300

    add item list funcionality

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner'
 
 export default class ItemList extends Component {
 
+  state = {
+    peopleList : null
+  }
+  
+  swapiService = new SwapiService();
+
+  componentDidMount () {
+    this.swapiService.getAllPeople()
+      .then(peopleList => {
+        this.setState({peopleList})
+      })
+  }
+
+  renderItems (arr) {
+    return arr.map(item => {
+      return (
+        <li className="list-group-item" 
+          key={item.id} 
+          onClick={() => this.props.onItemSelected(item.id)}>
+            {item.name}
+      </li>
+      )
+    })
+  }
+
   render() {
+
+    const {peopleList} = this.state;
+    if (!peopleList) {
+      return <Spinner/>
+    }
+    
+    const listItems = this.renderItems(peopleList);
+
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        {listItems}
       </ul>
     );
   }
