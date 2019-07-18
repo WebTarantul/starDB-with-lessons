eb1454e809f5189ca9065c6f2db53b0de71ac572
WebTarantul <tarstar1990@gmail.com>
2019-07-18 21:10:49 +0300

    refactoring at ItemList to arrow function

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -5,38 +5,28 @@ import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner'
 import withData from '../hoc-helper'
 
-
- class ItemList extends Component {
-
-  renderItems (arr) {
+const ItemList = (props) => {
+  const {children, onItemSelected, data} = props;
+  const renderItems = (arr) => {
     return arr.map(item => {
-
-      const label = this.props.children(item);
+      const label = children(item);
 
       return (
         <li className="list-group-item" 
           key={item.id} 
-          onClick={() => this.props.onItemSelected(item.id)}>
+          onClick={() => onItemSelected(item.id)}>
             {label}
       </li>
       )
     })
   }
 
-  render() {
-
-    const {data}  = this.props;
-    const listItems = this.renderItems(data);
-
-    return (
-      <ul className="item-list list-group">
-        {listItems}
-      </ul>
-    );
-  }
+  const listItems = renderItems(data);
+  return (
+    <ul className="item-list list-group">
+      {listItems}
+    </ul>
+  );
 }
 
-
-const {getAllPeople} = new SwapiService();
-
-export default withData(ItemList, getAllPeople);
+export default ItemList;
